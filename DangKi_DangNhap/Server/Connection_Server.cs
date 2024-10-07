4790329123e2e6ccd5da3d682373925bb9dea9ea
Hpgbao2204 <hpgbao@gmail.com>
2024-10-07 16:14:11 +0700

    Config Database + Server

--- DangKi_DangNhap/Server/Connection_Server.cs
+++ DangKi_DangNhap/Server/Connection_Server.cs
@@ -4,7 +4,7 @@ namespace Server
 {
     class Connection_Server
     {
-        private static string firebaseURL = "https://nt106-29f45-default-rtdb.firebaseio.com/";
+        private static string firebaseURL = "https://nt106-cce90-default-rtdb.firebaseio.com/";
         public static FirebaseClient GetFirebaseClient()
         {
             return new FirebaseClient(firebaseURL);
